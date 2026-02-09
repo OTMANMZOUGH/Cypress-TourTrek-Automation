d811526d8b54470145b11771c9b88b577664829e
otman <mzoughotman12@gmail.com>
2026-02-09 19:11:18 +0100

    Set baseUrl for E2E tests in Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://tour-trek-git-main-rida-bensalem-s-projects.vercel.app',
     // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
   },
