a01c09ace591f28f7d4e8572646cc59eacc5b0b0
otman <mzoughotman12@gmail.com>
2026-02-10 16:32:41 +0100

    SingUpTesting

--- cypress/e2e/Authentication/InvalidSingUp.cy.ts
+++ cypress/e2e/Authentication/InvalidSingUp.cy.ts
@@ -1,13 +1,8 @@
 describe('User Sign Up Flow - Tour Trek', () => {
 
-  beforeEach(() => {
+  beforeEach(function () {
     cy.visit('/');
+    cy.fixture('user').as('userData')
   });
-  it('1. Validate sign up with valid email address', () => {
-    cy.get('#user-menu').click();
-    cy.contains('Sign up').click();
-    cy.get('.rounded-t').should('have.text', 'Register');
 
-
-  });
 })
